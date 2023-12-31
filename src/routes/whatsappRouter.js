707be447672600d97d21b78c6cbe949b4c79b73e
JudisDev <judisson429@hotmail.com>
2023-12-31 14:45:01 -0300

    Feat - Criando o envio separado das informações
    - Mensagem e Status sendo enviado separado

--- src/routes/whatsappRouter.js
+++ src/routes/whatsappRouter.js
@@ -7,18 +7,110 @@ router.post('/mensagens-recebidas', async (req, res) => {
 
   const formattedJson = JSON.stringify(req.body, null, 2);
   console.log(formattedJson);
-  console.log("\n")
-  console.log("--------------------------------------------------------------------------------------")
-  console.log("\n")
+  console.log('\n');
+  console.log(
+    '--------------------------------------------------------------------------------------'
+  );
+  console.log('\n');
 
-  try {
-    await axios.post('http://localhost:3332/mensagens-recebidas-data', req.body);
-    console.log('Dados encaminhados para o servidor de destino');
-    res.status(200).send('Evento recebido e encaminhado!');
-  } catch (error) {
-    // console.error('Erro ao encaminhar dados:', error);
-    console.error('Erro ao encaminhar dados');
-    res.status(500).send('Erro ao encaminhar dados');
+  const { changes } = req.body.entry[0] || [];
+  console.log('changes', changes);
+  const { value } = req.body.entry[0].changes[0] || [];
+  // const idNumberClient = console.log("VAlue: ", value);
+  const { messages, message_echoes, contacts } = value;
+  let { statuses } = value;
+  statuses = statuses?.map((statusmsg) => {
+    let newStatus = { ...statusmsg };
+    newStatus.messageId = newStatus.id;
+    delete newStatus.id;
+    return newStatus;
+  });
+  const chatId = `${value.metadata?.phone_number_id}_${
+    contacts && contacts.length > 0
+      ? contacts[0].wa_id
+      : message_echoes && message_echoes.length > 0
+      ? message_echoes[0]?.to
+      : statuses && statuses.length > 0
+      ? statuses[0]?.recipient_id
+      : 'Id_Indefinido'
+  }`;
+
+  const chat = {
+    chatId: chatId,
+    messages: [
+      {
+        from:
+          messages && messages.length > 0
+            ? messages[0].from
+            : message_echoes && message_echoes.length > 0
+            ? message_echoes[0].from
+            : 'Erro no objeto From',
+        messageId:
+          messages && messages.length > 0
+            ? messages[0].id
+            : message_echoes && message_echoes.length > 0
+            ? message_echoes[0].id
+            : 'Erro no Objeto messageId',
+        timestamp:
+          messages && messages.length > 0
+            ? messages[0].timestamp
+            : message_echoes && message_echoes.length > 0
+            ? message_echoes[0].timestamp
+            : 'Erro no Objeto timestamp',
+        text:
+          messages && messages.length > 0
+            ? messages[0].text
+            : message_echoes && message_echoes.length > 0
+            ? message_echoes[0].text
+            : 'text',
+        type:
+          messages && messages.length > 0
+            ? messages[0].type
+            : message_echoes && message_echoes.length > 0
+            ? message_echoes[0].type
+            : 'type',
+        contact_name:
+          contacts && contacts.length > 0
+            ? contacts[0].profile?.name || 'Sem nome'
+            : 'Sem Nome',
+        contact_number:
+          contacts && contacts.length > 0
+            ? contacts[0].wa_id || 'Sem número Definido'
+            : undefined,
+      },
+    ],
+    field: changes[0].field,
+  };
+
+  const chatStatus = {
+    chatId: chatId,
+    statuses,
+  };
+
+  // console.log("Result:" ,result)
+  if (!statuses) {
+    try {
+      await axios.post('http://localhost:3332/menssages', chat);
+      console.log('Dados encaminhados para o servidor de destino');
+      res.status(200).send('Evento recebido e encaminhado!');
+    } catch (error) {
+      // console.error('Erro ao encaminhar dados:', error);
+      console.error('Erro ao encaminhar dados');
+      res
+        .status(500)
+        .send({ message: 'Erro ao encaminhar dados', error: error.toString() });
+    }
+  } else {
+    try {
+      await axios.post('http://localhost:3332/statuses', chatStatus);
+      console.log('Status encaminhados para o servidor de destino');
+      res.status(200).send('Evento recebido e encaminhado!');
+    } catch (error) {
+      console.error('Erro ao encaminhar status');
+      res
+        .status(500)
+        .send({ message: 'Erro ao encaminhar dados', error: error.toString() });
+    }
   }
 });
 
@@ -27,21 +119,21 @@ router.get('/mensagens-recebidas', (req, res) => {
   const VERIFY_TOKEN = 'meutoken';
 
   console.log('Requisição recebida get:', req.body);
-  
+
   // Parse dos parâmetros de query enviados pela verificação do webhook
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
-  
+
   // Checa se o token e o modo estão corretos
   if (mode && token) {
-      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-          // Responde com o challenge 'hub.challenge' enviado pelo WhatsApp
-          res.status(200).send(challenge);
-      } else {
-          // Responde com '403 Forbidden' se os tokens não batem
-          res.sendStatus(403);
-      }
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      // Responde com o challenge 'hub.challenge' enviado pelo WhatsApp
+      res.status(200).send(challenge);
+    } else {
+      // Responde com '403 Forbidden' se os tokens não batem
+      res.sendStatus(403);
+    }
   }
 });
 
